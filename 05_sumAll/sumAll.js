f8d95415e0e2dfb431ae636a772f07126a1f4628
cafesmire <cafesmire@gmail.com>
2024-10-29 20:36:44 -0400

    Fix the code to cover all the tests and pass
    Fixed the sumAll function to sum all numbers between the two given
    parameters, inclusive, and return the final sum. The function must
    check for types, negative values, and appropriate ranges.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let sum = 0;
+    const message = 'ERROR';
 
+    if((Number.isInteger(number1) && Number.isInteger(number2)) &&
+        number1 > 0 && number2 > 0){
+            if(number1 > number2){
+                return sumAll(number2, number1);
+            }
+            while(number1 <= number2){
+                sum += number1;
+                number1++;
+            }
+    }else return message;
+
+        return sum;
 };
 
 // Do not edit below this line
