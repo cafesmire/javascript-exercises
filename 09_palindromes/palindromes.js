23e3ee7ba5057dea4989c69b30101f0ec36b9389
cafesmire <cafesmire@gmail.com>
2024-10-31 08:47:51 -0400

    Repeat exercise to familiarize with regex
    Repeated exercise to familiarize with regular expressions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    const phrase = words.toLowerCase().replaceAll(/\W/g, '');
+    let start = 0;
+    let end = phrase.length-1;
+    let truth = true;
 
+    while(start < end){
+        if(phrase.charAt(start) != phrase.charAt(end)){
+            truth = false;
+            return truth;
+        }
+        start++;
+        end--;
+    }
+    return truth;
 };
 
 // Do not edit below this line
