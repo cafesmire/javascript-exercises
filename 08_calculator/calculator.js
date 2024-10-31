14ff65a5201ab5592b2d7b8aeebfd8374f19fe0d
cafesmire <cafesmire@gmail.com>
2024-10-31 08:29:50 -0400

    Rework calculator to optimize functions
    Fixed calculator functions to achieve results with better technique

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  const initialValue = 0;
+  return args.flat().reduce((total, element) =>
+    total + element, initialValue
+  );
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  const initialValue = 1;
+  return args.flat().reduce((total, element) => 
+  total * element, initialValue);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let total = 1;
+	if(x === 0 || x === 1){
+    return total;
+  }
+  let start = 1;
+  while(start <= x){
+    total *= start;
+    start++;
+  }
+  return total;
 };
 
 // Do not edit below this line
