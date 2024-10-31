68990bec54b634f7e2949a59e530abd5ebecf903
cafesmire <cafesmire@gmail.com>
2024-10-31 08:04:20 -0400

    Fix function to convert celcius and fahrenheit
    Fixed function to convert degrees to respective notation and applied
    the given rules to pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  //Formula for Fahrenheit to Celcius
+  const celcius = ((degrees-32) * (5/9));
+  //Return without decimals or return with one decimal to the right
+  return Number.isInteger(celcius) ? celcius : Number(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  //Formula for Celcius to Fahrenheit
+  const fahrenheit = ((degrees * (9/5)) + 32);
+  //Return without decimals or return with one decimal to the right
+  return Number.isInteger(fahrenheit) ? fahrenheit : Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
