f658c04f57acc6dc0ae53b2dcb2f366cb58f8928
cafesmire <cafesmire@gmail.com>
2024-11-01 13:18:31 -0400

    Rework memoization with fibonacci
    Reworked function to memoize number per instructions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num, memo) {
+    num = parseInt(num);
+    if(num < 0) return 'OOPS';
 
+    memo = memo || {};
+    if(memo[num]){
+        return memo[num];
+    }
+    //base case
+    if(num === 0) return 0;
+    if(num === 1) return 1;
+
+    return fibonacci(num-1, memo) + fibonacci(num-2, memo);
 };
 
 // Do not edit below this line
